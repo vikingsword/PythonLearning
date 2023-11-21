92c09971e03830813ca66d06506e5025f6b71707
vikingar <423141759@qq.com>
2023-11-21 22:37:54 +0800

    test

--- ByteOfPython/temp/test03.py
+++ ByteOfPython/temp/test03.py
@@ -7,6 +7,9 @@ class Student:
         print("name = ", self.name)
         print("age = ", self.age)
 
+    def __str__(self):
+        print("init str")
+
 
 if __name__ == '__main__':
     s = Student("zs", 29)
